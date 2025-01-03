332630c92f3fea8be8db59bb0361025d61cfbae3
Hussam Burqan <hussamhasmhasm@gmail.com>
2025-01-03 15:58:55 +0200

    25

---
+++ Assignment 25/BookController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class BookController : MonoBehaviour
+{
+    public float openSpeed = 100f;
+    public float maxOpenAngle = 120f;
+    public GameObject bookContent;
+
+    public float minScale = 0.1f;
+    public float maxScale = 1f;
+    public float scaleThreshold = 30f;
+
+    private float currentAngle = 0f;
+    private Vector3 originalScale;
+
+    void Start()
+    {
+        
+        bookContent.SetActive(false);
+        originalScale = bookContent.transform.localScale;
+        bookContent.transform.localScale = originalScale * minScale;
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            currentAngle = Mathf.Min(currentAngle + openSpeed * Time.deltaTime, maxOpenAngle);
+            if (currentAngle > 5)
+            {
+                bookContent.SetActive(true);
+
+            }
+        }
+        else
+        {
+
+            currentAngle = Mathf.Max(currentAngle - openSpeed * Time.deltaTime, 0);
+            if (currentAngle <5 ) {
+                bookContent.SetActive(false);
+
+            }
+        }
+
+        transform.localRotation = Quaternion.Euler(0, 0, -currentAngle);
+       
+        float scaleRatio = Mathf.Clamp01((currentAngle - scaleThreshold) / (maxOpenAngle - scaleThreshold));
+        float currentScaleFactor = Mathf.Lerp(minScale, maxScale, scaleRatio);
+
+        bookContent.transform.localScale = originalScale * currentScaleFactor;
+      
+    }
+}
---
+++ Assignment 25/SowrdController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    public float swingSpeed = 400f;
+    public float maxSwingAngle = 180f;
+    public float restingAngle = -33f;     
+
+    private float currentAngle;
+    private bool isSwinging = false;
+    private bool swingForward = true;
+
+    void Start()
+    {
+        currentAngle = restingAngle;
+        transform.localRotation = Quaternion.Euler(0, 0, currentAngle);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1) && !isSwinging)
+        {
+            isSwinging = true;
+            swingForward = true;
+        }
+
+        if (isSwinging)
+        {
+            if (swingForward)
+            {
+                currentAngle += swingSpeed * Time.deltaTime;
+                if (currentAngle >= maxSwingAngle)
+                {
+                    currentAngle = maxSwingAngle;
+                    swingForward = false;
+                }
+            }
+            else
+            {
+                currentAngle -= swingSpeed * Time.deltaTime;
+                if (currentAngle <= restingAngle)
+                {
+                    currentAngle = restingAngle;
+                    isSwinging = false;
+                }
+            }
+
+            transform.localRotation = Quaternion.Euler(0, 0, currentAngle);
+        }
+    }
+}
